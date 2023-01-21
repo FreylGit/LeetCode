350590245e95e003dde39262568c31a8edc7f7a7
ANDREY <41635263+FreylGit@users.noreply.github.com>
2023-01-21 23:12:39 +0300

    4

---
+++ 3/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+Дана строка s, найдите длину самой длинной 
+подстрока
+без повторяющихся символов.*/
+
+/*
+ Пример 1:
+    Ввод: s = "abcabcbb"
+    Вывод: 3
+    Объяснение: Ответ "abc", длина 3.
+*/
+
+Console.WriteLine(LengthOfLongestSubstring("aab"));
+
+static int LengthOfLongestSubstring(string s)
+{
+    int maxLength = 0;
+    if(s.Length == 1)
+    {
+        return 1;
+    }
+    int start = 1;
+    for (int i = start; i < s.Length; i++)
+    {
+        int currentLength = 1;
+        for (int j = i - 1; j >= 0; j--)
+        {
+            if (!s[i].Equals(s[j]))
+            {
+                currentLength++;
+            }
+            else
+            {
+                start = j + 1;
+                if(currentLength > maxLength)
+                {
+                    maxLength = currentLength;
+                }
+                break;
+            }
+           
+        }
+        if (i + 1 == s.Length)
+        {
+            if (currentLength > maxLength)
+            {
+                maxLength = currentLength;
+            }
+        }
+    }
+    return maxLength;
+}
---
+++ 4/Program.cs
@@ -0,0 +1,48 @@
+﻿/*Учитывая два отсортированных массива nums1и nums2 размер mи n соответственно, вернуть медиану двух отсортированных массивов.*/
+//TODO: Решение не оптимальное. Вернуться когда будет настроение и сделать оптимальное.
+int[] nums1 = { 1, 2 };
+int[] nums2 = { 3 };
+Console.WriteLine(FindMedianSortedArrays(nums1, nums2));
+
+
+static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+{
+	int[] result = new int[nums1.Length + nums2.Length];
+	int index = 0;
+	for (int i = 0; i < nums1.Length; i++)
+	{
+		result[index] = nums1[i];
+		index++;
+	}
+	for (int i = 0; i < nums2.Length; i++)
+	{
+		result[index] = nums2[i];
+		index++;
+	}
+    for (int i = 0; i < result.Length; i++)
+    {
+        for (int j = i + 1; j < result.Length; j++)
+        {
+            if (result[i] > result[j])
+            {
+                int tmp = result[i];
+                result[i] = result[j];
+                result[j] = tmp;
+            }
+        }
+    }
+
+    int len = result.Length / 2;
+    if (result.Length % 2 == 0)
+	{
+		
+		double sum = (result[len] + result[len - 1]) / 2.0;
+		return sum;
+	}
+	else
+	{
+        
+        return result[len];
+    }
+    
+}
